760e13cabae6e8d03184577fe56e0cb6fef54ede
Ankur Bhambri <ankur.bhambri@renewbuy.com>
2021-07-07 07:56:23 +0530

    changes

--- binary_tree.py
+++ binary_tree.py
@@ -1,19 +1,20 @@
+# Binary tree is a non linear data stucture
 class Node:
-
     def __init__(self, data):
-
         self.left = None
         self.right = None
         self.data = data
 
     def insert(self, data):
-        # Compare the new value with the parent node
+        # import ipdb; ipdb.set_trace()
         if self.data:
+            # if data smaller than root
             if data < self.data:
                 if self.left is None:
                     self.left = Node(data)
                 else:
                     self.left.insert(data)
+            # if data greater than root
             elif data > self.data:
                 if self.right is None:
                     self.right = Node(data)
@@ -22,38 +23,115 @@ class Node:
         else:
             self.data = data
 
-    def findval(self, lkpval):
-        # findval method to compare the value with nodes
-        if lkpval < self.data:
-            if self.left is None:
-                return str(lkpval)+" is not Found"
-            return self.left.findval(lkpval)
-        elif lkpval > self.data:
-            if self.right is None:
-                return str(lkpval)+" is not Found"
-            return self.right.findval(lkpval)
-        else:
-            return str(self.data) + " is found"
-
-    def PrintTree(self):
-        # Print the tree
+    def printTree(self):
         if self.left:
-            self.left.PrintTree()
-        print(self.data),
+            self.left.printTree()
+        print(self.data)
         if self.right:
-            self.right.PrintTree()
+            self.right.printTree()
 
 
-# Use the insert method to add nodes
-root = Node(27)
-root.insert(14)
-root.insert(35)
-root.insert(31)
-root.insert(10)
-root.insert(19)
+def findMaxDiameter(root):
+
+    if root is None:
+        return 0
+
+    l = findDiameter(root.left)
+    r = findDiameter(root.right)
+
+    temp = max(l, r) + 1
+    ans = max(temp, 1 + l + r)
+    findDiameter.res = max(findDiameter.res, ans)
+
+    return temp
 
 
-root.PrintTree()
+# This function returns overall maximum path sum in 'res'
+# And returns max path sum going through root
+def findMaxPathSum(root):
 
-print(root.findval(7))
-print(root.findval(14))
+    # Base Condition
+    if root is None:
+        return 0
+
+    # Hypothesis
+    l = findMaxPathSum(root.left)
+    r = findMaxPathSum(root.right)
+
+    # Induction
+    temp = max(max(l, r) + root.data, root.data)
+
+    # Max top represents the sum when the node under
+    # consideration is the root of the maxSum path and
+    # no ancestor of root are there in max sum path
+    ans = max(temp, l + r + root.data)
+
+    # Static variable to store the changes
+    # Store the maximum result
+    findMaxPathSum.res = max(findMaxPathSum.res, ans)
+
+    return temp
+
+
+# Return maximum path sum in tree with given root
+def findMaxSum(root):
+    # Initialize result
+    findMaxPathSum.res = 0
+
+    # Compute and return result
+    findMaxPathSum(root)
+    return findMaxPathSum.res
+
+
+def inorder(root):
+    if root:
+        inorder(root.left)
+        print(root.data)
+        inorder(root.right)
+
+
+def preorder(root):
+    if root:
+        print(root.data)
+        preorder(root.left)
+        preorder(root.right)
+
+
+def postorder(root):
+    if root:
+        preorder(root.left)
+        preorder(root.right)
+        print(root.data)
+
+
+if __name__ == "__main__":
+
+    # root = Node(10)
+    # root.insert(2)
+    # root.insert(10)
+    # root.insert(20)
+    # root.insert(1)
+    # root.insert(-25)
+    # root.insert(3)
+    # root.insert(4)
+
+    root = Node(10)
+    root.left = Node(2)
+    root.right = Node(10)
+    root.left.left = Node(20)
+    root.left.right = Node(1)
+    root.right.right = Node(-25)
+    root.right.right.left = Node(3)
+    root.right.right.right = Node(4)
+
+    print('Print tree')
+    root.printTree()
+
+    print('inorder')
+    inorder(root)
+    print('preorder')
+    preorder(root)
+    print('postorder')
+    postorder(root)
+    print('Max path sum in binary tree')
+    print(findMaxSum(root))
--- sorting.py
+++ sorting.py
@@ -1,79 +1,113 @@
-class Sorting:
 
-    def buuble_sort(self, arr):
-        ''' It's a simple sorting algorithm. This sorting algorithm is comparison-based algorithm 
-        in which each pair of adjacent elements is compared and the elements are swapped if they 
-        are not in order. This algorithm is not suitable for large data sets as its average and 
-        worst case complexity are of Ο(n^2) where n is the number of items.'''
+'''It's a simple sorting algorithm. This sorting algorithm is comparison-based algorithm
+in which each pair of adjacent elements is compared and the elements are swapped if they
+are not in order.'''
 
-        for i in range(len(arr)):
-            for k in range(len(arr)-1):
-                if arr[k] > arr[k+1]:
-                    arr[k], arr[k+1] = arr[k+1], arr[k]
+def recursive_bubble_sort(arr):  # O(n)
+    count = 0
+    for i in range(len(arr) - 1):
+        if arr[i] > arr[i + 1]:
+            arr[i], arr[i + 1] = arr[i + 1], arr[i]
+            count += 1
+    if count == 0:
+        return arr
+    else:
+        return recursive_bubble_sort(arr)
+
+def bubble_sort(arr):  # Ο(n^2)
+
+    for i in range(len(arr)):
+        for k in range(len(arr) - 1):
+            if arr[k] > arr[k + 1]:
+                arr[k], arr[k + 1] = arr[k + 1], arr[k]
+    return arr
+
+def insertion_sort(arr):
+    '''Insertion sort is a simple sorting algorithm that works the way we sort
+    playing cards in our hands.'''
+    for j in range(1, len(arr)):
+        value = arr[j]
+        i = j - 1
+        while i >= 0 and arr[i] > value:
+            arr[i + 1] = arr[i]
+            i -= 1
+        arr[i + 1] = value
+    return arr
+
+def selection_sort(arr):
+    '''The selection sort algorithm sorts an array by repeatedly finding the minimum
+    element (considering ascending order) from unsorted part and putting it at
+    the beginning. The algorithm maintains two subarrays in a given array.
+
+    1) The subarray which is already sorted.
+    2) Remaining subarray which is unsorted.
+
+    In every iteration of selection sort, the minimum element (considering ascending order)
+    from the unsorted subarray is picked and moved to the sorted subarray.'''
+
+    for i in range(len(arr)):
+        min_val = min(arr[i : len(arr)])
+        pos = arr.index(min_val)
+        if min_val < arr[i]:
+            arr[pos], arr[i] = arr[i], min_val
+    return arr
+
+def quickSort(arr):
+    n = len(arr)
+
+    if n < 2:
         return arr
 
-    def insertion_sort(self, arr):
-        ''' Insertion sort is a simple sorting algorithm that works the way we sort 
-        playing cards in our hands.'''
-        for i in range(1, len(arr)):
-            key = arr[i]
-            k = i-1
-            while k >= 0 and key < arr[k]:
-                arr[k+1] = arr[k]
-                k -= 1
-            arr[k+1] = key
-        return arr
+    # Position of the partitioning element
+    current_position = 0
 
-    def selection_sort(self, arr):
-        ''' The selection sort algorithm sorts an array by repeatedly finding the minimum 
-            element (considering ascending order) from unsorted part and putting it at 
-            the beginning. The algorithm maintains two subarrays in a given array.
+    # Partitioning loop
+    for i in range(1, n):
+        if arr[i] <= arr[0]:
+            current_position += 1
+            arr[i], arr[current_position] = arr[current_position], arr[i]
+            
+    # Brings pivot to it's appropriate position
+    arr[0], arr[current_position] = arr[current_position], arr[0]
 
-            1) The subarray which is already sorted.
-            2) Remaining subarray which is unsorted.
+    # Sorts the elements to the left of pivot
+    left = quickSort(arr[0:current_position])
+    # sorts the elements to the right of pivot
+    right = quickSort(arr[current_position + 1 : n])
 
-            In every iteration of selection sort, the minimum element (considering ascending order) 
-            from the unsorted subarray is picked and moved to the sorted subarray.'''
+    # Merging everything together
+    arr = left + [arr[current_position]] + right
 
-        for i in range(len(arr)):
-            min_val = min(arr[i:len(arr)])
-            pos = arr.index(min_val)
-            if min_val < arr[i]:
-                arr[pos], arr[i] = arr[i], min_val
-        return arr
+    return arr
 
-    # Quick Sort:
-    def partition(self, arr, l, r):
+def swapSort(arr):
 
-        pivot = arr[r]
-        i = l - 1
-        for j in range(l, r-1):
-            if arr[j] <= pivot:
-                i += 1
-                arr[i], arr[j] = arr[j], arr[i]
-        arr[i+1], arr[r] = arr[r], arr[i+1]
-        return i+1
-
-    def quickSort(self, arr, l, h):
-        if len(arr) == 1:
-            return arr
-        if l < h:
-            pi = self.partition(arr, l, h)
-            self.quickSort(arr, l, pi-1)
-            self.quickSort(arr, pi+1, h)
-
-        return arr
-
+    ''' swap sorting on the basis of index position values
+        like 1 index 2 is needed if values is present in
+        its indes then no swap '''
+    for i in range(len(arr)):
+        if arr[i] != arr[arr[i]-1]:
+            arr[arr[i]-1], arr[i] = arr[i], arr[arr[i]-1]
+    # position where wrong nos is present
+    for i in range(len(arr)):
+        if i+1 != arr[i]:
+            print('swap sort duplicate', arr[i], 'swap sort missing', i+1)
 
 if __name__ == "__main__":
 
     arr = [45, 3, 67, 56, 8, 0]
-    obj = Sorting()
 
-    # print('Bubble Sort', obj.buuble_sort(arr))
+    print('Recursive Bubble Sort', recursive_bubble_sort(arr))
 
-    # print('Insertion Sort', obj.insertion_sort(arr))
+    print('Bubble Sort', buuble_sort(arr))
 
-    # print('Selection Sort', obj.selection_sort(arr))
+    print('Insertion Sort', insertion_sort(arr))
 
-    # print("Quick Sort", obj.quickSort(arr, 0, len(arr)-1))
+    print('Selection Sort', selection_sort(arr))
+
+    print("Quick Sort", quickSort(arr))
+    
+    arr2 = [2,3,1,8,2,3,5,1]
+
+    swapSort(arr2)
+
