1cf260c93594a9479e87080f2b014982963eb624
Ankur Bhambri <ankur.bhambri@renewbuy.com>
2021-07-15 11:46:46 +0530

    changes

--- sorting.py
+++ sorting.py
@@ -1,7 +1,7 @@
-
 '''It's a simple sorting algorithm. This sorting algorithm is comparison-based algorithm
 in which each pair of adjacent elements is compared and the elements are swapped if they
 are not in order.'''
+
 
 def recursive_bubble_sort(arr):  # O(n)
     count = 0
@@ -14,6 +14,7 @@ def recursive_bubble_sort(arr):  # O(n)
     else:
         return recursive_bubble_sort(arr)
 
+
 def bubble_sort(arr):  # Ο(n^2)
 
     for i in range(len(arr)):
@@ -22,6 +23,7 @@ def bubble_sort(arr):  # Ο(n^2)
                 arr[k], arr[k + 1] = arr[k + 1], arr[k]
     return arr
 
+
 def insertion_sort(arr):
     '''Insertion sort is a simple sorting algorithm that works the way we sort
     playing cards in our hands.'''
@@ -29,10 +31,11 @@ def insertion_sort(arr):
         value = arr[j]
         i = j - 1
         while i >= 0 and arr[i] > value:
-            arr[i + 1] = arr[i]
+            arr[j] = arr[i]
             i -= 1
-        arr[i + 1] = value
+        arr[j] = value
     return arr
+
 
 def selection_sort(arr):
     '''The selection sort algorithm sorts an array by repeatedly finding the minimum
@@ -52,6 +55,7 @@ def selection_sort(arr):
             arr[pos], arr[i] = arr[i], min_val
     return arr
 
+
 def quickSort(arr):
     n = len(arr)
 
@@ -66,7 +70,7 @@ def quickSort(arr):
         if arr[i] <= arr[0]:
             current_position += 1
             arr[i], arr[current_position] = arr[current_position], arr[i]
-            
+
     # Brings pivot to it's appropriate position
     arr[0], arr[current_position] = arr[current_position], arr[0]
 
@@ -80,18 +84,20 @@ def quickSort(arr):
 
     return arr
 
+
 def swapSort(arr):
 
-    ''' swap sorting on the basis of index position values
-        like 1 index 2 is needed if values is present in
-        its indes then no swap '''
+    '''swap sorting on the basis of index position values
+    like 1 index 2 is needed if values is present in
+    its indes then no swap'''
     for i in range(len(arr)):
-        if arr[i] != arr[arr[i]-1]:
-            arr[arr[i]-1], arr[i] = arr[i], arr[arr[i]-1]
+        if arr[i] != arr[arr[i] - 1]:
+            arr[arr[i] - 1], arr[i] = arr[i], arr[arr[i] - 1]
     # position where wrong nos is present
     for i in range(len(arr)):
-        if i+1 != arr[i]:
-            print('swap sort duplicate', arr[i], 'swap sort missing', i+1)
+        if i + 1 != arr[i]:
+            print('swap sort duplicate', arr[i], 'swap sort missing', i + 1)
+
 
 if __name__ == "__main__":
 
@@ -99,15 +105,14 @@ if __name__ == "__main__":
 
     print('Recursive Bubble Sort', recursive_bubble_sort(arr))
 
-    print('Bubble Sort', buuble_sort(arr))
+    # print('Bubble Sort', buuble_sort(arr))
 
     print('Insertion Sort', insertion_sort(arr))
 
     print('Selection Sort', selection_sort(arr))
 
     print("Quick Sort", quickSort(arr))
-    
-    arr2 = [2,3,1,8,2,3,5,1]
+
+    arr2 = [2, 3, 1, 8, 2, 3, 5, 1]
 
     swapSort(arr2)
-
