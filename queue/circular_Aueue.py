4150afe07593638c8cf20f18e41cb0cf07f9b653
ankurbhambri <ankurwave09@gmail.com>
2022-01-07 23:53:30 +0530

    circular queue

--- queue/circular_Aueue.py
+++ queue/circular_Aueue.py
@@ -2,52 +2,52 @@
 
 class MyCircularQueue():
 
-    def __init__(self, k):
-        self.k = k
-        self.queue = [None] * k
-        self.head = self.tail = -1
+    def __init__(self, size):
+        self.size = size
+        self.queue = [None] * size
+        self.front = self.rear = -1
 
     # Insert an element into the circular queue
     def enqueue(self, data):
 
-        if ((self.tail + 1) % self.k == self.head):
+        if ((self.rear + 1) % self.size == self.front):
             print("The circular queue is full\n")
 
-        elif (self.head == -1):
-            self.head = 0
-            self.tail = 0
-            self.queue[self.tail] = data
+        elif (self.front == -1):
+            self.front = 0
+            self.rear = 0
+            self.queue[self.rear] = data
         else:
-            self.tail = (self.tail + 1) % self.k
-            self.queue[self.tail] = data
+            self.rear = (self.rear + 1) % self.size
+            self.queue[self.rear] = data
 
     # Delete an element from the circular queue
     def dequeue(self):
-        if (self.head == -1):
+        if (self.front == -1):
             print("The circular queue is empty\n")
 
-        elif (self.head == self.tail):
-            temp = self.queue[self.head]
-            self.head = -1
-            self.tail = -1
+        elif (self.front == self.rear):
+            temp = self.queue[self.front]
+            self.front = -1
+            self.rear = -1
             return temp
         else:
-            temp = self.queue[self.head]
-            self.head = (self.head + 1) % self.k
+            temp = self.queue[self.front]
+            self.front = (self.front + 1) % self.size
             return temp
 
     def printCQueue(self):
-        if(self.head == -1):
+        if(self.front == -1):
             print("No element in the circular queue")
 
-        elif (self.tail >= self.head):
-            for i in range(self.head, self.tail + 1):
+        elif (self.rear >= self.front):
+            for i in range(self.front, self.rear + 1):
                 print(self.queue[i], end=" ")
             print()
         else:
-            for i in range(self.head, self.k):
+            for i in range(self.front, self.size):
                 print(self.queue[i], end=" ")
-            for i in range(0, self.tail + 1):
+            for i in range(0, self.rear + 1):
                 print(self.queue[i], end=" ")
             print()
 
