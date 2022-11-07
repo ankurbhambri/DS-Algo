75747ac7f7ec7bc4b38954670b4b7ffef05ee306
Ankur Bhambri <ankurwave09@gmail.com>
2022-11-07 09:14:35 +0530

    bfs with a* approach

--- graph/A*.py
+++ graph/A*.py
@@ -8,116 +8,188 @@ all of the neighboring nodes, and then enters the node with the lowest value of 
 
 These values are calculated with the following formula: f(n) = g(n) + h(n)  '''
 
+from heapq import heappop, heappush
+
+
+def shortestPathBinaryMatrix(grid):
+    shortest_path = a_star_graph_search(
+        start = (0, 0), 
+        successor_function_obj = get_successor_function(grid),
+        heuristic_function_obj = get_heuristic(grid)
+    )
+    if shortest_path is None or grid[0][0] == 1:
+        return -1
+    else:
+        return len(shortest_path)
+
+
+def a_star_graph_search(start, successor_function_obj, heuristic_function_obj):
+
+    M, N = len(grid), len(grid[0])
+    visited = set()
+    came_from = dict()
+    distance = {start: 0}
+    frontier = PriorityQueue()
+    frontier.add(start)
+
+    while frontier:
+
+        node = frontier.pop()
+
+        if node in visited:
+            continue
+        # !! if coordinates reaches bottom right corner we got the answer hurray !!
+
+        if node == (M-1, N-1):
+            return reconstruct_path(came_from, start, node)
+        visited.add(node)
+
+        for successor in successor_function_obj(node):
+            frontier.add(
+                successor,
+                priority = distance[node] + 1 + heuristic_function_obj(successor)
+            )
+
+            if (successor not in distance
+                or distance[node] + 1 < distance[successor]):
+                distance[successor] = distance[node] + 1
+                came_from[successor] = node
+
+    return None
+
+
+def reconstruct_path(came_from, start, end):
+    """
+    >>> came_from = {'b': 'a', 'c': 'a', 'd': 'c', 'e': 'd', 'f': 'd'}
+    >>> reconstruct_path(came_from, 'a', 'e')
+    ['a', 'c', 'd', 'e']
+    """
+    reverse_path = [end]
+    while end != start:
+        end = came_from[end]
+        reverse_path.append(end)
+    return list(reversed(reverse_path))
+
+
+def get_successor_function(grid):
+    """
+    grid =  [[0, 0, 0],
+             [0, 1, 0],
+             [1, 0, 0]]
+    >>> f = get_successor_function(grid)
+    >>> sorted(f((1, 2)))
+    [(0, 1), (0, 2), (2, 1), (2, 2)]
+    >>> sorted(f((2, 1)))
+    [(1, 0), (1, 2), (2, 2)]
+    """
+    def get_clear_adjacent_cells(cell):
+        i, j = cell
+        return (
+            (i + a, j + b)
+            for a in (-1, 0, 1)
+            for b in (-1, 0, 1)
+            if a != 0 or b != 0
+            if 0 <= i + a < len(grid)
+            if 0 <= j + b < len(grid[0])
+            if grid[i + a][j + b] == 0
+        )
+    return get_clear_adjacent_cells
+
+
+def get_heuristic(grid):
+    """
+    >>> f = get_heuristic([[0, 0], [0, 0]])
+    >>> f((0, 0))
+    1
+    >>> f((0, 1))
+    1
+    >>> f((1, 1))
+    0
+    """
+    M, N = len(grid), len(grid[0])
+    (a, b) = goal_cell = (M - 1, N - 1)
+    def get_clear_path_distance_from_goal(cell):
+        (i, j) = cell
+        return max(abs(a - i), abs(b - j))
+    return get_clear_path_distance_from_goal
+
+class PriorityQueue:
+    
+    def __init__(self, iterable = []):
+        self.heap = []
+        for value in iterable:
+            heappush(self.heap, (0, value))
+    
+    def add(self, value, priority = 0):
+        heappush(self.heap, (priority, value))
+    
+    def pop(self):
+        priority, value = heappop(self.heap)
+        return value
+    
+    def __len__(self):
+        return len(self.heap)
+
+
+############################################# USing BFS #######################
 
 
 from collections import deque
 
 
-class Graph:
-    # example of adjacency list (or rather map)
-    # adjacency_list = {
-    # 'A': [('B', 1), ('C', 3), ('D', 7)],
-    # 'B': [('D', 5)],
-    # 'C': [('D', 12)]
-    # }
+def breadth_first_search(grid):
+    N = len(grid)
 
-    def __init__(self, adjacency_list):
-        self.adjacency_list = adjacency_list
+    def is_clear(cell):
+        return grid[cell[0]][cell[1]] == 0
 
-    def get_neighbors(self, v):
-        return self.adjacency_list[v]
+    def get_neighbors(cell):
+        (i, j) = cell
+        return (
+            (i + a, j + b)
+            for a in (-1, 0, 1)
+            for b in (-1, 0, 1)
+            if a != 0 or b != 0
+            if 0 <= i + a < N
+            if 0 <= j + b < N
+            if is_clear( (i + a, j + b) )
+        )
 
-    # heuristic function with equal values for all nodes
-    def h(self, n):
-        H = {
-            'A': 1,
-            'B': 1,
-            'C': 1,
-            'D': 1
-        }
+    start = (0, 0)
+    goal = (N - 1, N - 1)
 
-        return H[n]
+    queue = deque()
+    if is_clear(start):
+        queue.append(start)
+    visited = set()
+    path_len = {start: 1}
 
-    def a_star_algorithm(self, start_node, stop_node):
-        # open_list is a list of nodes which have been visited, but who's neighbors
-        # haven't all been inspected, starts off with the start node
-        # closed_list is a list of nodes which have been visited
-        # and who's neighbors have been inspected
-        open_list = set([start_node])
-        closed_list = set([])
+    while queue:
+        cell = queue.popleft()
+        if cell in visited:
+            continue
+        if cell == goal:
+            return path_len[cell]
+        visited.add(cell)
+        for neighbor in get_neighbors(cell):
+            if neighbor not in path_len:
+                path_len[neighbor] = path_len[cell] + 1
+            queue.append(neighbor)
 
-        # g contains current distances from start_node to all other nodes
-        # the default value (if it's not found in the map) is +infinity
-        g = {}
-
-        g[start_node] = 0
-
-        # parents contains an adjacency map of all nodes
-        parents = {}
-        parents[start_node] = start_node
-
-        while len(open_list) > 0:
-            n = None
-
-            # find a node with the lowest value of f() - evaluation function
-            for v in open_list:
-                if n == None or g[v] + self.h(v) < g[n] + self.h(n):
-                    n = v;
-
-            if n == None:
-                print('Path does not exist!')
-                return None
-
-            # if the current node is the stop_node
-            # then we begin reconstructin the path from it to the start_node
-            if n == stop_node:
-                reconst_path = []
-
-                while parents[n] != n:
-                    reconst_path.append(n)
-                    n = parents[n]
-
-                reconst_path.append(start_node)
-
-                reconst_path.reverse()
-
-                print('Path found: {}'.format(reconst_path))
-                return reconst_path
-
-            # for all neighbors of the current node do
-            for (m, weight) in self.get_neighbors(n):
-                # if the current node isn't in both open_list and closed_list
-                # add it to open_list and note n as it's parent
-                if m not in open_list and m not in closed_list:
-                    open_list.add(m)
-                    parents[m] = n
-                    g[m] = g[n] + weight
-
-                # otherwise, check if it's quicker to first visit n, then m
-                # and if it is, update parent data and g data
-                # and if the node was in the closed_list, move it to open_list
-                else:
-                    if g[m] > g[n] + weight:
-                        g[m] = g[n] + weight
-                        parents[m] = n
-
-                        if m in closed_list:
-                            closed_list.remove(m)
-                            open_list.add(m)
-
-            # remove n from the open_list, and add it to closed_list
-            # because all of his neighbors were inspected
-            open_list.remove(n)
-            closed_list.add(n)
-
-        print('Path does not exist!')
-        return None
+    return -1
 
 
-adjacency_list = {
-    'A': [('B', 1), ('C', 3), ('D', 7)],
-    'B': [('D', 5)],
-    'C': [('D', 12)]
-}
-graph1 = Graph(adjacency_list)
-print(graph1.a_star_algorithm('A', 'D'))
+grid = [
+	[0,0,0,1,0,0,1,0],
+	[0,0,0,0,0,0,0,0],
+	[1,0,0,1,1,0,1,0],
+	[0,1,1,1,0,0,0,0],
+	[0,0,0,0,0,1,1,1],
+	[1,0,1,0,0,0,0,0],
+	[1,1,0,0,0,1,0,0],
+	[0,0,0,0,0,1,0,0]
+]
+
+print(breadth_first_search(grid))
+
+print(shortestPathBinaryMatrix(grid))
