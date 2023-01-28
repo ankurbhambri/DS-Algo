7db18af6ffa3cee0f8c762ec9463597124e2f6ef
Ankur Bhambri <ankurwave09@gmail.com>
2023-01-28 12:06:56 +0530

    Update isPalindrome.py

--- recursion/isPalindrome.py
+++ recursion/isPalindrome.py
@@ -1,16 +1,21 @@
-# isPalindrome Solution
+# using iteration
+def is_palindrome(string):
+    for i in range(len(string) // 2):
+        if string[i] != string[-(i + 1)]:
+            return False
+    return True
 
+print(is_palindrome("racecar")) # True
+print(is_palindrome("hello")) # False
 
-def isPalindrome(strng):
-    if len(strng) == 0:
+# using recursion
+def is_palindrome(string):
+    if len(string) == 0 or len(string) == 1:
         return True
-    if strng[0] != strng[len(strng) - 1]:
+    if string[0] == string[-1]:
+        return is_palindrome(string[1:-1])
+    else:
         return False
-    return isPalindrome(strng[1 : len(strng) - 1])
 
-
-print(isPalindrome('awesome'))  # false
-print(isPalindrome('foobar'))  # false
-print(isPalindrome('tacocat'))  # true
-print(isPalindrome('amanaplanacanalpanama'))  # true
-print(isPalindrome('amanaplanacanalpandemonium'))  # false
+print(is_palindrome("racecar")) # True
+print(is_palindrome("hello")) # False
