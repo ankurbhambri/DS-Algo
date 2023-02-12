93e6238e7f402af01a58b7314db2860bb8e43c9d
Ankur Bhambri <ankurwave09@gmail.com>
2023-02-12 09:03:47 +0530

    changes

--- sliding_window/win_prb.py
+++ sliding_window/win_prb.py
@@ -1,5 +1,6 @@
 # https://www.hackerrank.com/challenges/sherlock-and-anagrams?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps
 
+
 def sherlockAndAnagrams(s):
     res = 0
     # win = 1
@@ -12,13 +13,15 @@ def sherlockAndAnagrams(s):
     #             d[sub] = 1 + d.get(sub, 0)
     #             res += d[sub]
     #     win += 1
-    #     n -= 1        
+    #     n -= 1
     # return res
     for i in range(1, len(s)):
         d = {}
-        for j in range(0, len(s) - i + 1): # till length of s - i + 1
+        for j in range(0, len(s) - i + 1):  # till length of s - i + 1
             # sliding window
-            sub = "".join(sorted(s[j : j + i])) # sorted return list
+            sub = "".join(sorted(s[j : j + i]))  # sorted return list
             d[sub] = 1 + d.get(sub, 0)
-            res += d[sub] - 1 # if there is only one anagram then it's not good
-    return res  
+            res += (
+                d[sub] - 1
+            )  # if there is only one anagram then it's not good
+    return res
