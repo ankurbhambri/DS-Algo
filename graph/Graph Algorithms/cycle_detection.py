aaa51ce527a39c4051860998dd7125b6601378b8
Ankur Bhambri <ankurwave09@gmail.com>
2023-02-14 19:47:58 +0530

    undirected and directed graph using DFS added

--- graph/Graph Algorithms/cycle_detection.py
+++ graph/Graph Algorithms/cycle_detection.py
@@ -3,13 +3,15 @@ In graph theory, a cycle graph or circular graph is a graph that consists of a s
 or in other words, some number of vertices connected in a closed chain
 '''
 
+# In Undirected graph case using DFS
 
-def has_cycle(graph, node, parent, visited):
 
-    visited[node] = True
+def has_cycle_undirected(graph, node, parent, visited):
+
+    visited.add(node)
     for nei in graph[node]:
-        if not visited[nei]:
-            if has_cycle(graph, nei, node, visited):
+        if nei not in visited:
+            if has_cycle_undirected(graph, nei, node, visited):
                 return True
         # already visited node and that node is not parent means cycle exists
         elif nei != parent:
@@ -18,11 +20,11 @@ def has_cycle(graph, node, parent, visited):
     return False
 
 
-def find_cycle(graph, n):
-    visited = [False] * n
+def find_cycle_undirected(graph, n):
+    visited = set()
     for i in range(n):
-        if not visited[i]:
-            if has_cycle(graph, i, -1, visited):
+        if i not in visited:
+            if has_cycle_undirected(graph, i, -1, visited):
                 return True
     return False
 
@@ -30,62 +32,45 @@ def find_cycle(graph, n):
 graph = {0: [1], 1: [2], 2: [3], 3: []}  # no cycle
 graph1 = {0: [1], 3: [0, 2], 2: [1], 1: []}  # cycle
 
-print("Cycle" if find_cycle(graph, 4) else "No cycle")  # no cycle
+print("Cycle" if find_cycle_undirected(graph, 4) else "No cycle")  # no cycle
 
-print("Cycle" if find_cycle(graph1, 4) else "No cycle")  # cycle
-
-
-'''
- Detect Cycle in a directed graph using colors
- Create a recursive function that takes the edge and color array (this can be also kept as a global variable)
-    a. Mark the current node as GREY.
-    b. Traverse all the adjacent nodes and if any node is marked GREY then return
-       true as a loop is bound to exist.
-    c. If any adjacent vertex is WHITE then call the recursive function for that node.
-       If the function returns true. Return true.
-    d. If no adjacent node is grey or has not returned true then mark the current
-       Node as BLACK and return false.
-'''
+print("Cycle" if find_cycle_undirected(graph1, 4) else "No cycle")  # cycle
 
 
-def dfs(u, color, graph):
-    # GRAY : This vertex is being processed (DFS
-    # 		 for this vertex has started, but not
-    # 		 ended (or this vertex is in function
-    # 		 call stack)
-    color[u] = "GRAY"
+# In  Directed graph case using DFS
 
-    for v in graph[u]:
 
-        if color[v] == "GRAY":
+def has_cycle_directed(graph, node, visited, dfs_visited):
+
+    visited.add(node)
+    dfs_visited.add(node)
+    for nei in graph[node]:
+        if nei not in visited:
+            if has_cycle_directed(graph, nei, visited, dfs_visited):
+                return True
+        # if node present in both set that means cycle exists.
+        elif nei in dfs_visited:
             return True
-
-        if color[v] == "WHITE" and dfs(v, color, graph) == True:
-            return True
-
-    color[u] = "BLACK"
+    dfs_visited.remove(node)
     return False
 
 
-def isCyclic(graph, V):
-    color = ["WHITE"] * V
-
-    for i in range(V):
-        if color[i] == "WHITE":
-            if dfs(i, color, graph) == True:
+def find_cycle_directed(graph, n):
+    visited = set()
+    dfs_visited = set()
+    for i in range(n):
+        if i not in visited:
+            if has_cycle_directed(graph, i, visited, dfs_visited):
                 return True
     return False
 
 
-# Driver program to test above functions
-g = {0: [1, 2], 1: [2], 2: [0, 3], 3: []}
+graph = {0: [1], 1: [2], 2: [3], 3: []}  # no cycle
+graph1 = {0: [1], 1: [2], 2: [0]}  # cycle
 
-print(
-    "Graph contains a cycle"
-    if isCyclic(g, 4)
-    else "Graph doesn't contain cycle"
-)
+print("Cycle" if find_cycle_directed(graph, 4) else "No cycle")  # no cycle
 
+print("Cycle" if find_cycle_directed(graph1, 3) else "No cycle")  # cycle
 
 '''
 It is not possible to find a cycle in a graph using a breadth-first search (BFS) algorithm,
