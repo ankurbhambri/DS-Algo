3f58ab09169a82b13ccaa6d8b2631173004bd8cd
Ankur Bhambri <ankurwave09@gmail.com>
2023-02-22 08:47:26 +0530

    chnages in code

--- graph/Graph Algorithms/tarjans_finding_bridges.py
+++ graph/Graph Algorithms/tarjans_finding_bridges.py
@@ -6,23 +6,32 @@ from collections import defaultdict
 
 
 def utils(graph, n):
-    disc = low = parent = [-1] * n
+
+    disc = [-1] * n
+    low = [-1] * n
+    parent = [-1] * n
     bridge = []
 
+    time = 0
+
     def dfs(u):
-        time = 0
+
+        nonlocal time
+
         disc[u] = low[u] = time
+
         time += 1
 
         for v in graph[u]:
-            if disc[v] == -1:
 
+            if disc[v] == -1:
                 parent[v] = u
+
                 dfs(v)
+
                 low[u] = min(low[u], low[v])
 
                 if low[v] > disc[u]:
-                    print(u, v)
                     bridge.append((u, v))
 
             elif v != parent[u]:
@@ -36,22 +45,22 @@ def utils(graph, n):
 
 
 graph = defaultdict(list)
-# graph[0].append(2)
-# graph[2].append(0)
-# graph[0].append(3)
-# graph[3].append(0)
-# graph[1].append(0)
-# graph[0].append(1)
-# graph[2].append(1)
-# graph[1].append(2)
-# graph[3].append(4)
-# graph[4].append(3)
-
+graph[0].append(1)
 graph[1].append(0)
 graph[0].append(2)
-graph[2].append(1)
+graph[2].append(0)
 graph[0].append(3)
+graph[3].append(0)
+graph[2].append(1)
+graph[1].append(2)
 graph[3].append(4)
+graph[4].append(3)
+
+# graph[1].append(0)
+# graph[0].append(2)
+# graph[2].append(1)
+# graph[0].append(3)
+# graph[3].append(4)
 
 
 print(utils(graph, 5))
