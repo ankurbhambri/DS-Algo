6ca37a9bf18781b019ba433a702952770ab7e345
Ankur Bhambri <ankurwave09@gmail.com>
2023-02-25 14:27:06 +0530

    Create subarray.py

---
+++ sliding_window/subarray.py
@@ -0,0 +1,39 @@
+# https://leetcode.com/problems/subarrays-with-k-different-integers/
+
+class Solution:
+
+    def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
+
+        freq = {}
+
+        l, r = 0, 0
+
+        res = 0
+
+        for i, x in enumerate(nums):
+
+            freq[x] = freq.get(x, 0) + 1
+
+            if len(freq) == k + 1:
+
+                # remove the distinct at start_k, move start_k, start
+                del freq[nums[r]]
+
+                r += 1
+
+                l = r
+
+            if len(freq) == k:
+
+                # update r and res (Notice: K >= 1)
+                
+                while freq[nums[r]] > 1:
+
+                    freq[nums[r]] -= 1
+
+                    r += 1
+
+                # size of window
+                res += r - l + 1
+
+        return res
