1a86fa4ff3a5cd1759660a8e08fdd4e6bdbc1d73
Ankur Bhambri <ankurwave09@gmail.com>
2023-02-25 13:57:43 +0530

    Update win_prb.py

--- sliding_window/win_prb.py
+++ sliding_window/win_prb.py
@@ -3,18 +3,6 @@
 
 def sherlockAndAnagrams(s):
     res = 0
-    # win = 1
-    # n = len(s) - 1
-    # while n > 0:
-    #     d = {}
-    #     for i in range(0, len(s)):
-    #         if i < len(s) - 1:
-    #             sub = "".join(sorted(s[i: i + win]))
-    #             d[sub] = 1 + d.get(sub, 0)
-    #             res += d[sub]
-    #     win += 1
-    #     n -= 1
-    # return res
     for i in range(1, len(s)):
         d = {}
         for j in range(0, len(s) - i + 1):  # till length of s - i + 1
