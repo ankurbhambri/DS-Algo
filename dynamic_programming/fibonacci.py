b90462eb72f108c7c1a32d0e595c4ba098b2985b
Ankur Bhambri <ankurwave09@gmail.com>
2023-03-08 17:56:48 +0530

    Update fibonacci.py

--- dynamic_programming/fibonacci.py
+++ dynamic_programming/fibonacci.py
@@ -2,10 +2,9 @@ def fibonacci(n):
     if n <= 1:
         return n
     
-    prev1 = 0
-    prev2 = 1
+    prev1, prev2 = 0, 1
     
-    for i in range(2, n+1):
+    for i in range(2, n + 1):
         prev1, prev2 = prev2, prev1 + prev2
         
     return prev2
