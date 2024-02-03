449607a82c904bd3a5f0af9a7750f9316f378e4e
ankur <ankurwave09@gmail.com>
2024-02-03 09:00:00 +0530

    chnages

--- tree/invert_binary_tree.py
+++ tree/invert_binary_tree.py
@@ -1,3 +1,13 @@
+# https://leetcode.com/problems/invert-binary-tree/
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 def invertTree(root):
 
     def helper(node):
@@ -14,3 +24,13 @@ def invertTree(root):
         return node
 
     return helper(root)
+
+
+obj = TreeNode(4)
+obj.left = TreeNode(2)
+obj.right = TreeNode(7)
+obj.left.left = TreeNode(1)
+obj.left.right = TreeNode(3)
+obj.right.left = TreeNode(6)
+obj.right.right = TreeNode(9)
+print(invertTree(obj))
