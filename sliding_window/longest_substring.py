2409e3136151308cd8e650914941305d6cc57d9d
ankur <ankurwave09@gmail.com>
2024-02-12 09:00:00 +0530

    changes

--- sliding_window/longest_substring.py
+++ sliding_window/longest_substring.py
@@ -47,51 +47,13 @@ def maximumUniqueSubarray(s):
 
 
 """
-https://leetcode.com/problems/longest-substring-with-at-most-two-distinct-characters/
-
-Given a string, find the length of the longest substring S that contains at most 2 distinct characters.
-
-"""
-
-
-class Solution:
-    def length_of_longest_substring_two_distinct(s):
-
-        l = 0
-        res = 0
-        freq = dict()
-
-        for r in range(len(s)):
-            w = s[r]
-
-            # calculating frequency here of all characters
-            if w in freq:
-                freq[w] += 1
-            else:
-                freq[w] = 1
-
-            while len(freq) > 2:
-                wl = s[l]
-
-                freq[wl] -= 1
-
-                l += 1
-
-                if freq[wl] == 0:
-
-                    del freq[wl]
-
-            res = max(res, r - l + 1)
-
-        return res
-
-
-"""
     https://leetcode.com/problems/longest-substring-with-at-most-k-distinct-characters/
 
     Given a string S, find the length of the longest substring T that contains at most k distinct characters.
 
     Note - same as above replace static value 2 with k only
+
+    Similar question - https://leetcode.com/problems/longest-substring-with-at-most-two-distinct-characters/ replace k with 2
 
 """
 
@@ -104,22 +66,20 @@ class Solution:
         freq = dict()
 
         for r in range(len(s)):
-            w = s[r]
-            if w in freq:
-                freq[w] += 1
-            else:
-                freq[w] = 1
-            while len(freq) > k:
-                wl = s[l]
 
-                freq[wl] -= 1
+            freq[s[r]] = 1 + freq.get(s[r], 0)
+
+            while len(freq) > k:
+
+                freq[s[l]] -= 1
 
                 l += 1
 
-                if freq[wl] == 0:
+                if freq[s[l]] == 0:
 
-                    del freq[wl]
+                    del freq[s[l]]
 
+            # windows length always calculated from (r - l + 1)
             res = max(res, r - l + 1)
 
         return res
