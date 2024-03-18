b3e3297ca2fdd08e9ce0c2f9d6fba8cdf2339c7f
ankur <ankurwave09@gmail.com>
2024-03-18 09:00:00 +0530

    changes

--- dynamic_programming/frog_jump.py
+++ dynamic_programming/frog_jump.py
@@ -2,26 +2,29 @@
 
 # Top down (Memoization)
 
+
 def frogJump(n: int, heights: List[int]) -> int:
-    
+
     memo = [-1] * n
-    
+
     def helper(i):
 
         if i == 0:
             return 0
-        
+
         if memo[i] != -1:
             return memo[i]
-        
+
         left = helper(i - 1) + abs(heights[i] - heights[i - 1])
 
-        right = helper(i - 2) + abs(heights[i] - heights[i - 2]) if i > 1 else float("inf")
+        right = (
+            helper(i - 2) + abs(heights[i] - heights[i - 2]) if i > 1 else float("inf")
+        )
 
         memo[i] = min(left, right)
 
         return memo[i]
-    
+
     return helper(n - 1)
 
 
@@ -29,7 +32,7 @@ def frogJump(n: int, heights: List[int]) -> int:
 def frogJump(n: int, heights: List[int]) -> int:
 
     dp = [0] * n
-    for i in range(1,  n):
+    for i in range(1, n):
         left = dp[i - 1] + abs(heights[i] - heights[i - 1])
         right = dp[i - 2] + abs(heights[i] - heights[i - 2]) if i > 1 else float("inf")
         dp[i] = min(left, right)
@@ -38,9 +41,9 @@ def frogJump(n: int, heights: List[int]) -> int:
 
 # Space optimized way
 def frogJump(n: int, heights: List[int]) -> int:
-    
+
     a = b = 0
-    for i in range(1,  n):
+    for i in range(1, n):
         left = b + abs(heights[i] - heights[i - 1])
         right = a + abs(heights[i] - heights[i - 2]) if i > 1 else float("inf")
         a, b = b, min(left, right)
--- dynamic_programming/frog_k_jumps.py
+++ dynamic_programming/frog_k_jumps.py
@@ -1,49 +1,50 @@
 # memoization
 def frog_k_jumps(n, heights):
 
-  memo = [-1] * n
+    memo = [-1] * n
 
-  def helper(i):
+    def helper(i):
 
-    if i == 0:
-      return 0
+        if i == 0:
+            return 0
 
-    if memo[i] != -1:
-      return dp[i]
+        if memo[i] != -1:
+            return dp[i]
 
-    res = float("inf")
+        res = float("inf")
 
-    for j in range(1, k):
-      if (i - j >= 0):
-        jump = helper(i - j) + abs(heights[i] + heights[i - j])
-        res = = min(res, jump)
-        memo[i] = res
+        for j in range(1, k):
+            if i - j >= 0:
+                jump = helper(i - j) + abs(heights[i] + heights[i - j])
+                res = min(res, jump)
+                memo[i] = res
 
-     return memo[i]
-  
-  return helper(n - 1)
+        return memo[i]
+
+    return helper(n - 1)
 
 
 # tabulation
 def frog_k_jumps(n, heights):
 
-  dp = [0] * n
+    dp = [0] * n
 
-  for i in range(1, n):
+    for i in range(1, n):
 
-    res = float("inf")
+        res = float("inf")
 
-    for j in range(1, k):
+        for j in range(1, k):
 
-      if (i - j >= 0):
+            if i - j >= 0:
 
-        jump = dp[i - j] + abs(heights[i] + heights[i - j])
+                jump = dp[i - j] + abs(heights[i] + heights[i - j])
 
-        res = min(res, jump)
+                res = min(res, jump)
 
-    dp[i] = res
+        dp[i] = res
 
-  return dp[n - 1]
+    return dp[n - 1]
+
 
 # space optimization
 # here space optimixation can only be done till k also if k == N then answer will be same as above
