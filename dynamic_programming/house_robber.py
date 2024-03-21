57deca731e44386a6e12f497eec582374b6f1aca
ankur <ankurwave09@gmail.com>
2024-03-21 09:00:00 +0530

    changes

--- dynamic_programming/house_robber.py
+++ dynamic_programming/house_robber.py
@@ -1,17 +1,17 @@
 # https://leetcode.com/problems/house-robber/
 class Solution:
-  def rob(self, nums: List[int]) -> int:
-    rob1, rob2 = 0, 0
-    for n in nums:
-        rob1, rob2 = rob2, max(n + rob1, rob2)
-    return rob2
+    def rob(self, nums: List[int]) -> int:
+        rob1, rob2 = 0, 0
+        for n in nums:
+            rob1, rob2 = rob2, max(n + rob1, rob2)
+        return rob2
 
 
 # https://leetcode.com/problems/house-robber-ii/
 class Solution:
     def rob(self, nums: List[int]) -> int:
         return max(nums[0], self.helper(nums[1:]), self.helper(nums[:-1]))
-        
+
     def helper(self, nums):
 
         rob1, rob2 = 0, 0
--- dynamic_programming/knap_sack_01.py
+++ dynamic_programming/knap_sack_01.py
@@ -5,6 +5,7 @@ from utils.functionExeutionDeco import measure
 @measure
 def knapSackRecursive(bag_cap, item_wt, item_val, N):
     memo = {}
+
     def helper(N, cap):
 
         if N == 0 or cap == 0:
@@ -12,22 +13,25 @@ def knapSackRecursive(bag_cap, item_wt, item_val, N):
 
         else:
 
-            curr_wt = item_wt[N - 1] # current item weight
-            curr_val = item_val[N - 1] # current item value
+            curr_wt = item_wt[N - 1]  # current item weight
+            curr_val = item_val[N - 1]  # current item value
 
-            if curr_wt <= cap: # if current item weight is lte to bag capacity
+            if curr_wt <= cap:  # if current item weight is lte to bag capacity
 
                 # there are two cases 1 take current item and add it's val or 2 skip current item
-                t1 = curr_val + helper(N - 1, cap - curr_wt) # 1. add current item with it's val
-                t2 = helper(N - 1, cap) # 2 or skip current item
+                t1 = curr_val + helper(
+                    N - 1, cap - curr_wt
+                )  # 1. add current item with it's val
+                t2 = helper(N - 1, cap)  # 2 or skip current item
 
                 return max(t1, t2)
 
-            else: # in case current item weight is more that bag capacity
+            else:  # in case current item weight is more that bag capacity
 
-                return helper(N - 1, cap) 
+                return helper(N - 1, cap)
 
     return helper(N, bag_cap)
+
 
 # Bottom Up iterative or tabulation approach
 # TC: O(N * W)
@@ -42,36 +46,40 @@ def knapSack(val, wt, W):
                 dp[w] = max(dp[w], dp[w - cw] + cv)
     return dp[W]
 
+
 # memoization approach for knap-sack
 @measure
 def knapSackCache(bag_cap, item_wt, item_val, N):
     memo = {}
+
     # two values are changing N and bag capacity
     def helper(N, cap):
 
         if N == 0 or cap == 0:
             return 0
-        
+
         # if (M, cap) already in memo
         elif (N, cap) in memo:
             return memo[(N, cap)]
 
         else:
 
-            curr_wt = item_wt[N - 1] # current item weight
-            curr_val = item_val[N - 1] # current item value
+            curr_wt = item_wt[N - 1]  # current item weight
+            curr_val = item_val[N - 1]  # current item value
 
-            if curr_wt <= cap: # if current item weight is lte to bag capacity
+            if curr_wt <= cap:  # if current item weight is lte to bag capacity
 
                 # there are two cases 1 take current item and add it's val or 2 skip current item
-                t1 = curr_val + helper(N - 1, cap - curr_wt) # 1. add current item with it's val
-                t2 = helper(N - 1, cap) # 2 or skip current item
+                t1 = curr_val + helper(
+                    N - 1, cap - curr_wt
+                )  # 1. add current item with it's val
+                t2 = helper(N - 1, cap)  # 2 or skip current item
 
-                c =  max(t1, t2)
+                c = max(t1, t2)
 
-            else: # in case current item weight is more that bag capacity
+            else:  # in case current item weight is more that bag capacity
 
-                c = helper(N - 1, cap) 
+                c = helper(N - 1, cap)
 
             memo[(N, cap)] = c
             return c
@@ -83,7 +91,7 @@ def knapSackCache(bag_cap, item_wt, item_val, N):
 def knapsackTabular(bag_cap, item_wt, item_val, N):
     dp = [[0] * (bag_cap + 1) for _ in range(N)]
     for i in range(N):
-        for j in range(bag_cap + 1): # j is cap
+        for j in range(bag_cap + 1):  # j is cap
             curr_wt = item_wt[i]
             curr_val = item_val[i]
             if i == 0:
@@ -94,20 +102,20 @@ def knapsackTabular(bag_cap, item_wt, item_val, N):
 
             else:
                 if curr_wt <= j:
-                    c1 = curr_val + dp[i - 1][j - curr_wt] # i - 1 is N - 1
+                    c1 = curr_val + dp[i - 1][j - curr_wt]  # i - 1 is N - 1
                     c2 = dp[i - 1][j]
                     dp[i][j] = max(c1, c2)
                 else:
                     dp[i][j] = dp[i - 1][j]
     return dp[N - 1][bag_cap]
-                    
+
 
 # Driver code
 if __name__ == "__main__":
-    N = 3 # weights and values of N items
+    N = 3  # weights and values of N items
     item_val = [12, 10, 6]
     item_wt = [2, 1, 4]
-    bag_cap = 5    
+    bag_cap = 5
 
     print(knapSack(item_val, item_wt, bag_cap))
     print(knapSackCache(bag_cap, item_wt, item_val, N))
