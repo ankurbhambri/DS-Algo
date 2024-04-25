af05f4ba8f797f358078ec92c596847bf53f9401
ankur <ankurwave09@gmail.com>
2024-04-25 09:00:00 +0530

    changes

--- dynamic_programming/shortest_common_supersequence.py
+++ dynamic_programming/shortest_common_supersequence.py
@@ -9,13 +9,14 @@ def shortestCommonSupersequence(str1, str2, m, n):
                 dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
 
     # till this lcs solution
-    val = dp[m][n] 
+    val = dp[m][n]
 
     # just subracted matching sequence from both string
-    a = len(str1) - val 
+    a = len(str1) - val
     b = len(str2) - val
-    
-    # atlast added matching sequence in both string 
+
+    # atlast added matching sequence in both string
     return a + b + val
+
 
 print(shortestCommonSupersequence("abcd", "xycd", 4, 4))
