c98e9e5264b763126788ff5356a20a203e6cd125
ankur <ankurwave09@gmail.com>
2024-05-06 09:00:00 +0530

    changes

--- sliding_window/subarray_with_k_diff_integers.py
+++ sliding_window/subarray_with_k_diff_integers.py
@@ -34,33 +34,3 @@ def subarraysWithKDistinct(nums, k):
 
 
 print(subarraysWithKDistinct(nums=[1, 2, 1, 2, 3], k=2))
-
-
-def subarraysWithKDistinct(nums, k):
-    freq = {}
-
-    l = 0
-
-    res = 0
-
-    for r in range(len(nums)):
-
-        freq[nums[r]] = freq.get(nums[r], 0) + 1
-
-        if len(freq) > k:
-            freq[nums[l]] -= 1
-            if freq[nums[l]] == 0:
-                del freq[nums[l]]
-            l += 1
-
-        if len(freq) == k:
-            while freq[nums[l]] > 1:
-                freq[nums[l]] -= 1
-                l += 1
-
-        res += r - l + 1
-
-    return res
-
-
-print(subarraysWithKDistinct(nums=[1, 2, 1, 2, 3], k=2))
