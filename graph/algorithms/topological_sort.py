eb58b5b613d1ef23a30073a31ce56beb834e33cb
Ankur Bhambri <ankurwave09@gmail.com>
2024-05-11 22:03:17 +0530

    Update topological_sort.py

--- graph/algorithms/topological_sort.py
+++ graph/algorithms/topological_sort.py
@@ -9,7 +9,6 @@
 - Topological Sorting for a graph is not possible if the graph is not a DAG(Direct Acyclic Graph).
 """
 
-
 def topologicalSort(n, graph):
     adj = {c: [] for c in range(n)}
 
@@ -22,26 +21,26 @@ def topologicalSort(n, graph):
     def dfs(node):
         # if there is cycle means no possible to do topological sort
         if node in cycle:
-            return False
+            return True
 
         if node in visit:
-            return True
+            return False
 
         # First add in cycle then remove it.
         cycle.add(node)
         visit.add(node)
 
         for ch in adj[node]:
-            if not dfs(ch):
-                return False
+            if dfs(ch):
+                return True
 
         cycle.remove(node)  # backtrack
         res.append(node)
 
-        return True
+        return False
 
     for cur in range(n):
-        if dfs(cur) == False:
+        if dfs(cur):
             return []
 
     return res
@@ -50,76 +49,41 @@ def topologicalSort(n, graph):
 print(topologicalSort(4, [[1, 0], [2, 0], [3, 1], [3, 2]]))
 
 
-# Khan's Algorithm for BFS to create topological sort using indegree method.
-
-# Indegree means how many nodes directly connecting to a node.
-
-
-def topologicalSort2(n, graph):
-    adj = {c: [] for c in range(n)}
-    in_degree = {i: 0 for i in range(n)}
-
-    for n1, n2 in graph:
-        adj[n1].append(n2)
-        in_degree[n2] += 1  # finding indegree for nodes
-
-    res = []
-    visit = set()
-
-    # Taking all the edges who has no incoming edge into it.
-    q = [k for k, v in in_degree.items() if v == 0]
-
+# Cycle Detection in Directed Graph using BFS(Kahn's Algorithm) indegree method
+def khan_algo(graph):
+    indegree = {i:0 for i in graph}
+    
+    for i in range(len(graph)): # need n here
+        for node in graph[i]:
+            indegree[node] += 1
+    
+    q = []
+    
+    for i in range(len(graph)): # let's add those nodes whose indegree is 0
+        if indegree[i] == 0:
+            q.append(i)
+    
+    res = []   
     while q:
         node = q.pop(0)
         res.append(node)
-        for ch in adj[node]:
-            if ch not in visit:
-                in_degree[ch] -= 1
-                if not in_degree[ch]:
-                    q.append(ch)
-                    visit.add(ch)
+        
+        for nei in adj[node]:
+            indegree[nei] -= 1
+            if indegree[nei] == 0:
+                q.append(nei)
+                
     return res
 
-
-print(topologicalSort2(4, [[1, 0], [2, 0], [3, 1], [3, 2]]))
-
-
-# Cycle Detection in Directed Graph using BFS(Kahn's Algorithm) indegree method
-
-
-def Kahn_algo(n, graph):
-    adj = {c: [] for c in range(n)}
-    in_degree = {i: 0 for i in range(n)}
-
-    for n1, n2 in graph:
-        adj[n1].append(n2)
-        in_degree[n2] += 1
-
-    visit = set()
-
-    # Taking all the edges who has no incoming edge into it.
-    q = [k for k, v in in_degree.items() if v == 0]
-
-    count = 0  # only this is the change in above code
-
-    while q:
-        node = q.pop(0)
-        count += 1  # keep counting
-        for ch in adj[node]:
-            if ch not in visit:
-                in_degree[ch] -= 1
-                if not in_degree[ch]:
-                    q.append(ch)
-                    visit.add(ch)
-
-    # If i'm able to generate the topological sort that means count will be
-    # equal to n where n will be total number of nodes
-    if count == n:  # if counting equals to
-        return False
-    return True
-
-
-print(Kahn_algo(4, [[1, 0], [2, 0], [3, 1], [3, 2]]))
+adj = {
+    0: [],
+    1: [],
+    2: [3],
+    3: [1],
+    4: [0, 1],
+    5: [0, 2]
+}
+print(khan_algo(adj))
 
 """
 Note:-
