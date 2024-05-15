837ee15ed30695d3c1744faff526089fce4c3a77
Ankur Bhambri <ankurwave09@gmail.com>
2024-05-15 10:52:43 +0530

    Update alien_dictionary.py

--- graph/problems/Graph problems/alien_dictionary.py
+++ graph/problems/Graph problems/alien_dictionary.py
@@ -39,3 +39,31 @@ def alienOrder(words):
 
 
 print(alienOrder(["wrt", "wrf", "er", "ett", "rftt"]))
+
+
+# using Khan's algo
+
+def alienOrder(words):
+    
+    adj = {c: set() for w in words for c in w}
+    indegree = {c: 0 for w in words for c in w}
+    
+    for i in range(1, len(words)):
+        w1, w2 = words[i - 1], words[i]
+        for j in range(min(len(w1), len(w2))):
+            if w1[j] != w2[j]:
+                adj[w1[j]].add(w2[j])
+                indegree[w2[j]] += 1
+                break
+     
+    print(adj, indegree)     
+    q = [k for k, v in indegree.items() if v == 0]
+    res = []
+    while q:
+        node = q.pop(0)
+        res.append(node)
+        for nei in adj[node]:
+            indegree[nei] -= 1
+            if indegree[nei] == 0:
+                q.append(nei)
+    return res
