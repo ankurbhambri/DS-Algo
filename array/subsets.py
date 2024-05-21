8a8ade6805150ab12003690027df30b810c48293
Ankur Bhambri <ankurwave09@gmail.com>
2024-05-21 11:09:33 +0530

    Create subsets.py

---
+++ array/subsets.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/subsets/
+# TC - O(N * S), SC - O(N)
+
+def subsets(nums)
+    res = [[]]
+    
+    for num in nums:
+        # [[]] * [1], [[], [1]] * [2] ........
+        res += [i + [num] for i in res]
+
+    return res
