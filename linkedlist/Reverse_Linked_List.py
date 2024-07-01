00168d6853eb44db455f404bb7a46266e44cde54
ankur <ankurwave09@gmail.com>
2024-07-01 09:00:00 +0530

    new code

---
+++ linkedlist/Reverse_Linked_List.py
@@ -0,0 +1,30 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def reverseList(head):
+
+    prev = None
+    while head:
+        nxt = head.next
+        head.next = prev
+        prev = head
+        head = nxt
+
+    # concise way
+
+    # prev = None
+    # while head:
+    #     head.next, prev, head = prev, head, head.next
+
+    return prev
+
+
+obj = ListNode(1)
+obj.next = ListNode(2)
+obj.next.next = ListNode(3)
+obj.next.next.next = ListNode(4)
+obj.next.next.next.next = ListNode(5)
+print(reverseList(obj))
