29206b3e267f8416479e712c270e301a106295b3
ankur <ankurwave09@gmail.com>
2024-08-13 21:23:23 +0530

    changes

--- dynamic_programming/fibonacci.py
+++ dynamic_programming/fibonacci.py
@@ -1,3 +1,26 @@
+def fib(n, dp):  # memoization recursive method
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    if n not in dp:
+        dp[n] = fib(n - 1, dp) + fib(n - 2, dp)
+    return dp[n]
+
+
+print(fib(7))
+
+
+def fib_tab(n):  # tabular approach
+    tb = [0, 1]
+    for i in range(2, n + 1):
+        tb.append(tb[i - 1] + tb[i - 2])
+    return tb[n]
+
+
+print(fib_tab(7))
+
+
 def fibonacci(n):
     if n <= 1:
         return n
@@ -10,5 +33,4 @@ def fibonacci(n):
     return prev2
 
 
-print(fibonacci(13))
 print(fibonacci(7))
--- recursion/fibonacci.py
+++ recursion/fibonacci.py
@@ -1,20 +1,7 @@
-def fib(n, dp):  # memoization recursive method
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    if n not in dp:
-        dp[n] = fib(n - 1, dp) + fib(n - 2, dp)
-    return dp[n]
+def fib(n):
+    if n <= 1:
+        return n
+    return fib(n - 1) + fib(n - 2)
 
 
-def fib_tab(n):  # tabular approach
-    tb = [0, 1]
-    for i in range(2, n + 1):
-        tb.append(tb[i - 1] + tb[i - 2])
-    return tb[n]
-
-
-dp = {0: 0, 1: 1, 2: 1}
-print(fib(7, dp))
-print(fib_tab(7))
+print(fib(7))
