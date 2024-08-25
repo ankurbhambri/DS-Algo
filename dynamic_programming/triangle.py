ae66ae52e8e6744eba2138feae2cb62c2e9e6ae9
ankur <ankurwave09@gmail.com>
2024-08-25 16:35:47 +0530

    new code and changes

--- dynamic_programming/triangle.py
+++ dynamic_programming/triangle.py
@@ -1,48 +1,39 @@
-# memoization (top down)
+# https://leetcode.com/problems/triangle/submissions/
+
+
+# (top-down approach)
 # TC = O(N * N)
-# SC = O(N * N) + Stack Space
-def triangle(triangle, n):
-    memo = [[-1 for _ in range(n)] for _ in range(n)]
+# SC = O(1)
+def triangle_top_down(triangle):
+    n = len(triangle)
+    for i in range(1, n):
+        for j in range(len(triangle[i])):
+            if j == 0:
+                triangle[i][j] += triangle[i - 1][j]
+            elif j == len(triangle[i - 1]):
+                triangle[i][j] += triangle[i - 1][j - 1]
+            else:
+                triangle[i][j] = (
+                    min(triangle[i - 1][j - 1], triangle[i - 1][j]) + triangle[i][j]
+                )
 
-    def helper(i, j):
-        if memo[i][j] != -1:
-            return memo[i][j]
-
-        if i == n - 1:
-            memo[i][j] = triangle[i][j]
-            return memo[i][j]
-
-        down = triangle[i][j] + helper(i + 1, j)
-        dig = triangle[i][j] + helper(i + 1, j + 1)
-        memo[i][j] = min(down, dig)
-        return memo[i][j]
-
-    helper(0, 0)
-    return memo[0][0]
+    return min(triangle[-1])
 
 
-# tabulation (bottom up)
+print(triangle_top_down([[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]]))
+
+
+# (bottom-up approach)
 # TC = O(N * N)
-# SC = O(N * N)
-def minimumPathSum(triangle, n):
-    dp = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
-    dp[n - 1] = triangle[n - 1]  # copy last row no way to compute down from it
+# SC = O(1)
+def triangle_bottom_up(triangle):
+    n = len(triangle)
     for i in range(n - 2, -1, -1):
-        col = triangle[i]  # col size in changing
-        for j in range(len(col) - 1, -1, -1):
-            dp[i][j] = triangle[i][j] + min(dp[i + 1][j], dp[i + 1][j + 1])
-    return dp[0][0]
+        for j in range(len(triangle[i])):
+            triangle[i][j] = (
+                min(triangle[i + 1][j], triangle[i + 1][j + 1]) + triangle[i][j]
+            )
+    return triangle[0][0]
 
 
-# space optimization
-# TC = O(N * N)
-# SC = O(N)
-def minimumPathSum(triangle, n):
-    prev = triangle[n - 1]  # copy last row no way to compute down from it
-    for i in range(n - 2, -1, -1):
-        col = triangle[i]
-        tmp = [0] * len(col)
-        for j in range(len(col) - 1, -1, -1):
-            tmp[j] = triangle[i][j] + min(prev[j], prev[j + 1])
-        prev = tmp
-    return prev[0]
+print(triangle_bottom_up([[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]]))
