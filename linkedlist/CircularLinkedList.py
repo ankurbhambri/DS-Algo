37372303ee0056f13b2386efde042d4bb004c38c
ankur <ankurwave09@gmail.com>
2024-09-29 10:16:33 +0530

    new codes

--- linkedlist/CircularLinkedList.py
+++ linkedlist/CircularLinkedList.py
@@ -1,4 +1,4 @@
-""" Circluar Linked List"""
+""" Circular Linked List """
 
 
 class Node:
@@ -9,146 +9,119 @@ class Node:
 
 class CircularLL:
     def __init__(self):
-        self.head = None  # first of node
-        self.tail = None  # last of node
-
-    def insertCll(self, data, loc=None):
-
-        newNode = Node(data)
-        if self.head is None:
-            self.head = newNode
-            self.tail = newNode
-            self.tail.next = self.head
-        # Insert at first
-        elif loc == 0:
-            newNode.next = self.head
-            self.head = newNode
-            self.tail.next = self.head
-        # Insert at last
-        elif loc == -1:
-            newNode.next = self.tail.next  # or self.head basically this is head node
-            self.tail.next = newNode
-            self.tail = newNode
-        # Insert at given location
-        else:
-            cur = self.head
-            i = 0
-            while i < loc - 1:
-                cur = cur.next
-                i += 1
-
-            nextNode = cur.next
-            cur.next = newNode
-            newNode.next = nextNode
-
-            if cur == self.tail:
-                self.tail = newNode
-
-    def searchnode(self, val):
-
-        if not self.node:
-            return "In bound"
-
-        elif val is not None:
-            cur = self.head
-            while cur:
-                if cur.val == val:
-                    return cur.val
-                if cur == self.tail:
-                    return "No value found"
-                cur = cur.next
-
-        return "The val does not exist in this list"
-
-    def deletenode(self, loc):
-
-        if self.head is None:
-            return "In bound"
-
-        elif loc == 0:
-            self.head = self.head.next
-            self.tail.next = self.head
-
-        elif loc == -1:
-            prev = None
-            cur = self.head
-            while cur != self.tail:
-                prev = cur
-                cur = cur.next
-            self.tail = prev
-            self.tail.next = self.head
-
-        else:
-            i = 0
-            cur = self.head
-            while i < loc - 1:
-                cur = cur.next
-                i += 1
-
-            cur.next = cur.next.next
-
-    def deleteEntire(self):
-
         self.head = None
-        self.tail.next = None
         self.tail = None
 
-    # Reverse Circular Linked List
-    def reverseLL(self):
+    def insert(self, data, loc=None):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = self.tail = new_node
+            self.tail.next = self.head
+        elif loc == 0:
+            new_node.next = self.head
+            self.head = new_node
+            self.tail.next = self.head
+        elif loc == -1:
+            new_node.next = self.head
+            self.tail.next = new_node
+            self.tail = new_node
+        else:
+            cur = self.head
+            for _ in range(loc - 1):
+                cur = cur.next
+            new_node.next = cur.next
+            cur.next = new_node
+            if cur == self.tail:
+                self.tail = new_node
 
+    def search(self, val):
+        if not self.head:
+            return "List is empty"
         cur = self.head
-        prev = None
-        while cur:
+        while True:
+            if cur.val == val:
+                return cur.val
+            if cur == self.tail:
+                break
+            cur = cur.next
+        return "Value not found"
+
+    def delete(self, loc):
+        if self.head is None:
+            return "List is empty"
+        if loc == 0:
+            if self.head == self.tail:
+                self.head = self.tail = None
+            else:
+                self.head = self.head.next
+                self.tail.next = self.head
+        elif loc == -1:
+            if self.head == self.tail:
+                self.head = self.tail = None
+            else:
+                cur = self.head
+                while cur.next != self.tail:
+                    cur = cur.next
+                cur.next = self.head
+                self.tail = cur
+        else:
+            cur = self.head
+            for _ in range(loc - 1):
+                cur = cur.next
+            cur.next = cur.next.next
+
+    def delete_entire(self):
+        self.head = self.tail = None
+
+    def reverse(self):
+        if not self.head or self.head == self.tail:
+            return
+        prev, cur = None, self.head
+        while True:
             nxt = cur.next
             cur.next = prev
             prev = cur
             if cur == self.tail:
-                # prev = self.tail and nxt = self.head so nxt.next is prev
-                nxt.next = prev
-                self.head = prev
-                self.tail = nxt
-                return prev
+                break
             cur = nxt
+        self.head.next = prev
+        self.head, self.tail = self.tail, self.head
 
-    # Reverse Doubly Linked List with in a range
-    def rangeReverse(self, left, right):
-
+    def range_reverse(self, left, right):
+        if left == right:
+            return
         l_prev, cur = None, self.head
         for _ in range(left - 1):
             l_prev, cur = cur, cur.next
-
         prev = None
         for _ in range(right - left + 1):
             nxt = cur.next
             cur.next = prev
             prev = cur
             cur = nxt
+        if l_prev:
+            l_prev.next.next = cur
+            l_prev.next = prev
+        else:
+            self.head.next = cur
+            self.head = prev
 
-        l_prev.next.next = cur
-        l_prev.next = prev
-
-    # Traverse Linked List
     def traverse(self):
+        if not self.head:
+            return
         cur = self.head
-        while cur:
+        while True:
             print(cur.val)
             if cur == self.tail:
                 break
             cur = cur.next
 
 
-# 0 means start 1 means add after first and -1 means add at last
-
+# Example usage
 obj = CircularLL()
-
-obj.insertCll(0, 0)
-obj.insertCll(1, 1)
-obj.insertCll(2, 2)
-obj.insertCll(3, 3)
-obj.insertCll(4, -1)
-
-# provide +1 in location coz we have 1 decrement
-# obj.deletenode(1)
-# obj.reverseLL()
-# obj.rangeReverse(2, 4)
+obj.insert(0, 0)
+obj.insert(1, 1)
+obj.insert(2, 2)
+obj.insert(3, 3)
+obj.insert(4, -1)
 obj.traverse()
