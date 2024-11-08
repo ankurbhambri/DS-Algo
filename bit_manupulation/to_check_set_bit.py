09af29aa61a3ae3baf60076c3e70372b66e2eda2
Ankur Bhambri <ankurwave09@gmail.com>
2024-11-08 10:13:54 +0530

    Update to_check_set_bit.py

--- bit_manupulation/to_check_set_bit.py
+++ bit_manupulation/to_check_set_bit.py
@@ -1,5 +1,5 @@
 def is_bit_set(number, n):
-    # Shift 1 to the left by (n - 1) positions to create a mask for the nth bit
+    # Shift 1 to the left by (n - 1) positions to create a mask for the nth bit.
     mask = 1 << (n - 1)
     # Perform bitwise AND to check if the nth bit is set
     return (number & mask) != 0
