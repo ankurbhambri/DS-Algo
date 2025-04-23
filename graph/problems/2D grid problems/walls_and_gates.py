d3e6a2811223fbccbc5be153b87a7a82d8e4e558
Ankur Bhambri <ankurwave09@gmail.com>
2025-04-23 11:31:43 +0530

    new changes

--- graph/problems/2D grid problems/walls_and_gates.py
+++ graph/problems/2D grid problems/walls_and_gates.py
@@ -1,46 +1,42 @@
 # https://leetcode.com/problems/walls-and-gates/
-import collections
+from collections import deque
+
+def wallsAndGates(grid):
+
+    m, n = len(grid), len(grid[0])
+    queue = deque()
+    
+    # treasure chests (0) in queue
+    for i in range(m):
+        for j in range(n):
+            if grid[i][j] == 0:
+                queue.append((i, j))
+    
+    # bfs
+    # Directions: up, down, left, right
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    
+    while queue:
+        x, y = queue.popleft()
+        
+        # 4 neighbors
+        for dx, dy in directions:
+            new_x, new_y = x + dx, y + dy
+            
+            # Check if new coordinates are valid and cell is INF
+            if 0 <= new_x < m and 0 <= new_y < n and grid[new_x][new_y] == 2147483647:
+
+                # distance = current cell ka distance + 1
+                grid[new_x][new_y] = grid[x][y] + 1
+                queue.append((new_x, new_y))
 
 
-class Solution:
-    def wallsAndGates(self, rooms):
-        m, n = len(rooms), len(rooms[0])
-        q = collections.deque()
-        paths = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-        empty = 2147483647
+grid = [
+    [2147483647, -1, 0, 2147483647],
+    [2147483647, 2147483647, 2147483647, -1],
+    [2147483647, -1, 2147483647, -1],
+    [0, -1, 2147483647, 2147483647]
+]
 
-        # Adding gates co-ordinates to the Queue for peforming BFS
-        for i in range(m):
-            for j in range(n):
-                if rooms[i][j] == 0:
-                    q.append((i, j))
-
-        # Computing BFS for all gates
-        step = 1
-        while q:
-            for _ in range(len(q)):
-                r, c = q.popleft()
-                for x, y in paths:
-                    nr, nc = r + x, c + y
-                    # skipping out of bounds, obstacles and already updated cells
-                    if not (0 <= nr < m and 0 <= nc < n) or rooms[nr][nc] != empty:
-                        continue
-                    rooms[nr][nc] = step
-                    q.append((nr, nc))
-            # Incrememting step by 1 after all cells belonging to specific level in the queue are covered.
-            step += 1
-
-        return rooms
-
-
-obj = Solution()
-print(
-    obj.wallsAndGates(
-        [
-            [2147483647, -1, 0, 2147483647],
-            [2147483647, 2147483647, 2147483647, -1],
-            [2147483647, -1, 2147483647, -1],
-            [0, -1, 2147483647, 2147483647],
-        ]
-    )
-)
+print(wallsAndGates(grid))
+print(grid)  # [[3, -1, 0, 1], [2, 1, 2, -1], [1, -1, 3, -1], [0, -1, 4, 5] ]
