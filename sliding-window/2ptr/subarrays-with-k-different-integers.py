5248212b23e77bbefacc302efa71276915e64d57
Ankur Bhambri <ankurwave09@gmail.com>
2025-08-15 15:48:56 +0530

    changes

--- sliding-window/2ptr/subarrays-with-k-different-integers.py
+++ sliding-window/2ptr/subarrays-with-k-different-integers.py
@@ -1,30 +1,44 @@
 # https://leetcode.com/problems/subarrays-with-k-different-integers/
 
-# This problem is a variation of the sliding window problem where we need to count the number of subarrays that contain exactly K distinct integers.
-
 '''
-Sometimes the problem says “EXACTLY K distinct” (e.g., “Count subarrays with exactly K distinct numbers”).
 
-We do this: exactlyK = atMostK(K) - atMostK(K-1)
+Question: We need to count the number of subarrays that contains exactly K distinct integers.
 
-Because: All subarrays with at most K distinct - All subarrays with at most (K-1) distinct = Subarrays with exactly K distinct.
+Notes:
+    
+    Sometimes the problem says “EXACTLY K distinct” (e.g., “Count subarrays with exactly K distinct numbers”).
+
+    We do this: exactlyK = atMostK(K) - atMostK(K-1)
+
+    Because: All subarrays with at most K distinct - All subarrays with at most (K-1) distinct = Subarrays with exactly K distinct.
 '''
 
 
 class Solution:
     def subarraysWithKDistinct(self, nums, K: int) -> int:
+
         def atMost(k):
+
             l = 0
             res = 0
             count = {}
+
             for r in range(len(nums)):
+
                 count[nums[r]] = count.get(nums[r], 0) + 1
+
                 while len(count) > k:
+
                     count[nums[l]] -= 1
+
                     if count[nums[l]] == 0:
+
                         del count[nums[l]]
+
                     l += 1
+
                 res += (r - l + 1)
+
             return res
 
         return atMost(K) - atMost(K-1)
