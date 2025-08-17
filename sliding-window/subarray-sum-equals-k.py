21fed0e93b9eccd719eaafa71271d39f55008ec3
Ankur Bhambri <ankurwave09@gmail.com>
2025-08-17 18:39:39 +0530

    new coes

--- sliding-window/subarray-sum-equals-k.py
+++ sliding-window/subarray-sum-equals-k.py
@@ -26,8 +26,56 @@ print(subarraySum([1, 1, 1], 2))  # Output: 2 -> [1, 1] and [1, 1]
 print(subarraySum([1, 2, 3], 3))  # Output: 2 -> [1, 2] and [3]
 print(subarraySum([1, 2, 1, 2, 1], 3))  # Output: 4 -> [1, 2], [2, 1], [1, 2], [1, 2]
 
+# Variant: Return True if there exists a subarray with sum equal to k, otherwise return False.
+def hasSubarraySum(nums, k):
 
-# https://leetcode.com/problems/maximum-size-subarray-sum-equals-k/description/
+    sum_val = 0
+    prefixSum = {0: 1}
+
+    for i in nums:
+        sum_val += i
+
+        if (sum_val - k) in prefixSum:
+            return True
+
+        prefixSum[sum_val] = 1 + prefixSum.get(sum_val, 0)
+
+    return False
+
+
+print(hasSubarraySum([5], 5)) # True
+print(hasSubarraySum([1, 1, 1], 2)) # True
+print(hasSubarraySum([-1, -2, -3, -4], -15)) # False
+print(hasSubarraySum([3, 4, 7, 2, -3, 1, 4, 2, 1, -14], -10)) # False
+
+
+# Variant: Without extra space (Just check True or False)
+def subarraySumExistsPositiveNums(nums, k):
+
+    l = 0
+    window_sum = 0
+
+    for r in range(len(nums)):
+
+        window_sum += nums[r]
+
+        while window_sum > k:
+            window_sum -= nums[l]
+            l += 1
+        
+        if window_sum == k:
+            return True
+
+    return False
+
+
+print(subarraySumExistsPositiveNums([1, 1, 1], 2)) # True
+print(subarraySumExistsPositiveNums([1, 2, 3], 3)) # True
+print(subarraySumExistsPositiveNums([1, 3, 5, 23, 2], 7)) # False
+print(subarraySumExistsPositiveNums([100, 101, 102, 103], 2)) # False
+
+
+# similar: https://leetcode.com/problems/maximum-size-subarray-sum-equals-k/description/
 
 # Longest Subarray With Sum K
 def maxSubArrayLen(nums, k):
@@ -52,55 +100,6 @@ print(maxSubArrayLen([1, -1, 5, -2, 3], 3))  # Output: 4
 print(maxSubArrayLen([-2, -1, 2, 1], 1))  # Output: 2
 print(maxSubArrayLen([1, 2, 3], 3))  # Output: 2
 print(maxSubArrayLen([1, 2, 3], 6))  # Output: 3
-
-
-# Variant: Return True if there exists a subarray with sum equal to k, otherwise return False.
-def hasSubarraySum(nums, k):
-
-    sum_val = 0
-    prefixSum = {0: 1}
-
-    for i in nums:
-        sum_val += i
-
-        if (sum_val - k) in prefixSum:
-            return True
-
-        prefixSum[sum_val] = 1 + prefixSum.get(sum_val, 0)
-
-    return False
-
-
-print(hasSubarraySum([5], 5)) # True
-print(hasSubarraySum([1, 1, 1], 2)) # True
-print(hasSubarraySum([-1, -2, -3, -4], -15)) # False
-print(hasSubarraySum([3, 4, 7, 2, -3, 1, 4, 2, 1, -14], -10)) # False
-
-
-# Without extra space (Just check True or False)
-def subarraySumExistsPositiveNums(nums, k):
-
-    l = 0
-    window_sum = 0
-
-    for r in range(len(nums)):
-
-        window_sum += nums[r]
-
-        while window_sum > k:
-            window_sum -= nums[l]
-            l += 1
-        
-        if window_sum == k:
-            return True
-
-    return False
-
-
-print(subarraySumExistsPositiveNums([1, 1, 1], 2)) # True
-print(subarraySumExistsPositiveNums([1, 2, 3], 3)) # True
-print(subarraySumExistsPositiveNums([1, 3, 5, 23, 2], 7)) # False
-print(subarraySumExistsPositiveNums([100, 101, 102, 103], 2)) # False
 
 
 # similar: https://leetcode.com/problems/binary-subarrays-with-sum/
--- sliding-window/variable-size/max-consecutive-ones.py
+++ sliding-window/variable-size/max-consecutive-ones.py
@@ -35,3 +35,113 @@ print(Solution().longestOnes([1,1,0,0,1,1,0,1], 2))  # Output: 6
 print(Solution().longestOnes([0,0,1,1,0,0,1,1], 1))  # Output: 4
 print(Solution().longestOnes([1,0,1,0,1,0], 3))  # Output: 6
 print(Solution().longestOnes([1,1,1,1,1], 0))  # Output: 5
+
+
+# Variant
+
+'''
+
+Given an m x n character matrix days of 'W' and 'H' where W represents a work day you may take off and H represents a holiday, 
+return the longest possible vacation of consecutive days you can take if you are allowed at most PTO days off.
+
+Example 1:
+
+    Input: days = [['W', 'H', 'W'], ['H', 'W', 'H'], ['W', 'H', 'W']],
+    PTO = 3
+    Output: 7
+
+Example 2:
+
+    Input: days = [['W', 'H', 'H', 'W', 'W'], ['H', 'W', 'W', 'W', 'W']],
+    PTO = 2
+    Output: 5
+
+Constraints:
+
+    m == days. length
+    • n == days [i]. length
+    • 1 <= m <= 10^3
+    • 1 <= n <= 10^3
+    days[i][j] is either W or H.
+    0 <= PTO <= 10^7
+
+'''
+
+def solution(days, pto):
+
+    l = 0
+    w = 0
+    res = 0
+
+    rows, cols = len(days), len(days[0])
+
+    # traverse like a list with a single loop, we can flatten indexing: row = i // cols, col = i % cols
+    def getIndex(idx):
+        return days[idx // cols][idx % cols]
+
+    n = rows * cols # flatten the matrix
+
+    for i in range(n):
+
+        if getIndex(i) == 'W':
+            w += 1
+
+        while w > pto:
+
+            if getIndex(l) == 'W':
+                w -= 1
+
+            l += 1
+
+        res = max(res, i - l + 1)
+
+    return res
+
+
+print(solution([['W', 'H', 'W'], ['H', 'W', 'H'], ['W', 'H', 'W']], 3))  # Output: 7
+print(solution([['W', 'H', 'H', 'W', 'W'], ['H', 'W', 'W', 'W', 'W']], 2))  # Output: 5
+
+
+# What if PTO is decimal? like: 2.5, or 3.2, In that case we can simply find the max window and a check from left or right side is 'W' day then add that decimal extension as well.
+
+def solution(days, pto):
+
+    l = 0
+    w = 0
+    res = 0
+
+    pto_ext = pto - int(pto)  # integer part of PTO
+    pto = int(pto)  # integer part of PTO
+
+    rows, cols = len(days), len(days[0])
+
+    n = rows * cols
+
+    def getIndex(idx):
+        return days[idx // cols][idx % cols]
+
+    for i in range(n):
+
+        if getIndex(i) == 'W':
+            w += 1
+
+        while w > pto:
+
+            if getIndex(l) == 'W':
+                w -= 1
+
+            l += 1
+
+        ext = 0.0
+        if l > 0 and getIndex(l - 1) == 'W' or i < n - 1 and getIndex(i + 1) == 'W':
+            ext = pto_ext
+
+        res = max(res, i - l + 1 + ext)
+
+    return res
+
+
+print(solution([['W', 'H', 'W'], ['H', 'W', 'H'], ['W', 'H', 'W']], 3.5))  # Output: 7.5
+print(solution([['W', 'H', 'W'], ['H', 'W', 'H'], ['W', 'H', 'H']], 4.5))  # Output: 9.0
+print(solution([['W', 'H', 'H', 'W', 'W'], ['H', 'W', 'W', 'W', 'W']], 2.2))  # Output: 5.2
+print(solution([['H', 'H', 'H', 'H', 'H'], ['H', 'H', 'H', 'H', 'H']], 1.5))  # Output: 10.0
