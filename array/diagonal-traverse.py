6d0a8099ed202dc70eb464263de052ad23f6eea5
Ankur Bhambri <ankurwave09@gmail.com>
2025-08-25 08:05:05 +0530

    array code added

---
+++ array/diagonal-traverse.py
@@ -0,0 +1,34 @@
+# https://leetcode.com/problems/diagonal-traverse/
+
+
+# https://leetcode.com/problems/diagonal-traverse/solutions/7118941/simple-short-clean-diagonal-grouping-with-parity-based-reversal-python/
+
+
+class Solution:
+    def findDiagonalOrder(self, mat):
+
+        res = []
+        n, m = len(mat), len(mat[0])
+
+        for d in range(n + m - 1):
+
+            i = 0 if d < m else d - m + 1
+            j = d if d < m else m - 1
+
+            diag = []
+            while i < n and j >= 0:
+                diag.append(mat[i][j])
+                i += 1
+                j -= 1
+
+            # reverse even diagonals
+            if d % 2 == 0:
+                diag.reverse()
+
+            res.extend(diag)
+
+        return res
+
+
+print(Solution().findDiagonalOrder([[1,2,3],[4,5,6],[7,8,9]]))
+print(Solution().findDiagonalOrder([[1,2],[3,4]]))
--- dynamic-programming/Subsequence-Substring-Distance-Common/longest-common-subsequence.py
+++ dynamic-programming/Subsequence-Substring-Distance-Common/longest-common-subsequence.py
@@ -87,6 +87,49 @@ def printlongestCommonSubsequence(str1, str2):
 
 print(printlongestCommonSubsequence("ABC", "AC"))
 
+# Optimized LCS Solution (Length + Sequence)
+
+# Time = O(m × n) and Space = O(m × n) for this version
+
+def lcs(a: str, b: str):
+    m, n = len(a), len(b)
+
+    # DP table: (m+1) x (n+1)
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+    # Fill DP table
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if a[i - 1] == b[j - 1]:
+                dp[i][j] = 1 + dp[i - 1][j - 1]
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+    # Reconstruct sequence
+    i, j = m, n
+    lcs_seq = []
+    while i > 0 and j > 0:
+        if a[i - 1] == b[j - 1]:
+            lcs_seq.append(a[i - 1])
+            i -= 1
+            j -= 1
+        elif dp[i - 1][j] > dp[i][j - 1]:
+            i -= 1
+        else:
+            j -= 1
+
+    lcs_seq.reverse()
+    return dp[m][n], ''.join(lcs_seq)
+
+
+# Example usage:
+a = "AGGTAB"
+b = "GXTXAYB"
+length, sequence = lcs(a, b)
+print("LCS length:", length)
+print("LCS sequence:", sequence)
+
+
 
 # Similar problem - https://leetcode.com/problems/shortest-common-supersequence/
 # Shortest Common Supersequence
