976f0385a6cf7c4ce0a522f6f6bd758ec0d9908b
Ankur Bhambri <ankurwave09@gmail.com>
2025-11-30 07:59:22 +0530

    new code

---
+++ stack/exclusive-time-of-functions.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+class Solution:
+    def exclusiveTime(self, n: int, logs):
+
+        res, st, prev = [0] * n, deque(), 0
+
+        for log in logs:
+
+            id, type, time = log.split(":")
+            id, time = int(id), int(time)
+
+            if type == "start":
+                if st:
+                    res[st[-1]] += time - prev
+                st.append(id)
+                prev = time
+
+            else:
+                res[st.pop()] += time - prev + 1
+                prev = time + 1
+
+        return res
+
+
+print(Solution().exclusiveTime(2, ["0:start:0", "1:start:2", "1:end:5", "0:end:6"]))
+print(Solution().exclusiveTime(1, ["0:start:0","0:start:2","0:end:5","0:start:6","0:end:6","0:end:7"]))
