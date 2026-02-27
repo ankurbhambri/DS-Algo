33bf86248ce9d83e3437c292cdba188088b2cd36
Ankur Bhambri <ankurwave09@gmail.com>
2026-02-27 10:37:57 +0530

    changhes

--- dynamic-programming/1D-2D-DP/word-break-I-II.py
+++ dynamic-programming/1D-2D-DP/word-break-I-II.py
@@ -1,3 +1,5 @@
+from typing import List
+
 # https://leetcode.com/problems/word-break/
 
 
@@ -53,3 +55,35 @@ def wordBreakII(s, wordDict):
 
 
 print(wordBreakII("catsanddog", ["cat", "cats", "and", "sand", "dog"]))
+
+# Memoisation
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        wordSet = set(wordDict)
+        memo = {}
+
+        def dfs(start):
+            if start in memo:
+                return memo[start]
+            
+            if start == len(s):
+                return [""]   # base case
+            
+            res = []
+            
+            for end in range(start + 1, len(s) + 1):
+                word = s[start:end]
+                
+                if word in wordSet:
+                    sub_sentences = dfs(end)
+                    
+                    for sub in sub_sentences:
+                        if sub:
+                            res.append(word + " " + sub)
+                        else:
+                            res.append(word)
+            
+            memo[start] = res
+            return res
+        
+        return dfs(0)
