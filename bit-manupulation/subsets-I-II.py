059e09c2f6d334ff78d6b7913ac8e97924d97912
Ankur Bhambri <ankurwave09@gmail.com>
2026-03-11 22:46:38 +0530

    new code

---
+++ bit-manupulation/subsets-I-II.py
@@ -0,0 +1,113 @@
+# https://leetcode.com/problems/subsets/
+
+
+# Backtracking approach (preferred)
+
+# Tc: O(n * 2^n)
+# SC: O(n) recursion stack
+
+class Solution:
+    def subsets(self, nums):
+
+        res = []
+
+        def backtrack(start, path):
+            res.append(path[:])  # add every path (even empty)
+            for i in range(start, len(nums)):
+                path.append(nums[i])
+                backtrack(i + 1, path)
+                path.pop()
+
+        backtrack(0, [])
+        return res
+
+
+# Another approach using bit manipulation (not preferred)
+
+# TC: O(n * 2^n)
+# SC: O(n * 2^n) for storing all subsets
+
+class Solution:
+    def subsets(self, nums):
+        res = []
+        n = len(nums)
+        for i in range(2 ** n):
+            tmp = []
+            for j in range(n):
+                if i & (1 << j):
+                    tmp.append(nums[j])
+            res.append(tmp)
+        return res
+
+print(Solution().subsets([1, 2, 3]))  # [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]
+
+# Another way for above problem
+
+# TC: O(n * 2^n)
+# SC: O(n * 2^n)
+
+def subsets(nums):
+
+    res = [[]]
+
+    for num in nums:
+        # [[]] * [1], [[], [1]] * [2] ........
+        res += [i + [num] for i in res]
+
+    return res
+
+print(subsets([1, 2, 3]))  # [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]
+
+
+# https://leetcode.com/problems/subsets-ii/
+
+
+# Backtracking approach (preferred)
+
+# Tc: O(n * 2^n) -> [O(n log n + n * 2^n) for sorting + backtracking, but 2^n  >>  log n, the dominant term is: O(n * 2^n)]
+# SC: O(n) + O(2^n * n)
+
+class Solution:
+    def subsetsWithDup(self, nums):
+
+        res = []
+        nums.sort()  # required to handle duplicates
+
+        def backtrack(start, path):
+            res.append(path[:])
+            for i in range(start, len(nums)):
+                if i > start and nums[i] == nums[i - 1]:
+                    continue  # skip duplicate at same tree level
+                path.append(nums[i])
+                backtrack(i + 1, path)
+                path.pop()
+
+        backtrack(0, [])
+        return res
+
+
+# Another approach using bit manipulation (not preferred)
+
+# TC: O(2^n * n log n)
+# SC: O(n * 2^n)
+class Solution:
+    def subsetsWithDup(self, nums):
+
+        res = []
+        seen = set()
+        n = len(nums)
+
+        for i in range(1 << n): # 1 << n is equal to 2 ** n
+            tmp = []
+            for j in range(n):
+                if i & (1 << j):
+                    tmp.append(nums[j])
+
+            tmp.sort()
+            if tuple(tmp) not in seen:
+                res.append(tmp)
+                seen.add(tuple(tmp))
+
+        return res
+
+print(Solution().subsetsWithDup([1, 2, 2]))  # [[], [1], [2], [1, 2], [2, 2], [1, 2, 2]]
